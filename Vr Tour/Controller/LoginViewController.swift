956b94ba425268eb959b6f6fd0743d737316d52e
Abdalah Omar <abdalahomar@Abdalahs-MacBook-Pro.local>
2019-11-21 02:30:53 +0200

    First push

---
+++ Vr Tour/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vr Tour
+//
+//  Created by Abdalah Omar on 11/21/19.
+//  Copyright © 2019 Hello Tomorrow. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
